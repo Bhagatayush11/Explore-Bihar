5265368f8530f078bcf023f4bce6795c3b589a01
Bhagatayush11 <155670291+Bhagatayush11@users.noreply.github.com>
2024-01-12 03:41:56 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,15 @@
+function func1(){
+    window.location.assign("patna.html");
+}
+function func2(){
+    window.location.assign("gaya.html");
+}
+function func3(){
+    window.location.assign("rajgir.html");
+}
+function func4(){
+    window.location.assign("buxar.html");
+}
+function func5(){
+    window.location.assign("rohtas.html");
+}
